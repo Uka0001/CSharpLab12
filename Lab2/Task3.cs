b772a15675a3490f99bef9ffcf86ea09ab92e077
Uka0001 <ukraine0001@gmail.com>
2022-11-02 20:24:39 +0200

    task3 fix

--- Lab2/Task3.cs
+++ Lab2/Task3.cs
@@ -7,33 +7,57 @@
  * в числі кілька разів, то повинні бути розглянуті останні з однакових цифр
  */
 
-/*
 class Task3
 {
     static void whatNumerOnRight(int x, int a, int b)
     {
-        while (x > 0)
+        Boolean resultA = false;
+        Boolean resultB = false;
+        int temp = x;
+        while (temp > 0)
         {
-            int rem = x % 10;
-            if (rem == a)
+            if (temp % 10 == a)
             {
-                Console.WriteLine("number a - " + a + " on the right of x earlier than b - " + b);
-                break;
-            }
-            else if (rem == b)
-            {
-                Console.WriteLine("number b - " + b + " on the right of x earlier than a - " + a);
-                break;
-            }
-            else
-            {
-                Console.WriteLine("There is no number a - " + a + " or b - " + b + " in last digit of x ");
+                resultA = true;
             }
 
-            x = x / 10;
+            if (temp % 10 == b)
+            {
+                resultB = true;
+            }
+
+            temp /= 10;
+        }
+
+        if (resultA == false || resultB == false)
+        {
+            Console.WriteLine("a or b missing try again");
+        }
+        else
+        {
+            while (x > 0)
+            {
+                int rem = x % 10;
+                if (rem == a)
+                {
+                    Console.WriteLine("number a - " + a + " on the right of x earlier than b - " + b);
+                    break;
+                }
+                else if (rem == b)
+                {
+                    Console.WriteLine("number b - " + b + " on the right of x earlier than a - " + a);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("There is no number a - " + a + " or b - " + b + " in last digit of x ");
+                }
+
+                x = x / 10;
+            }
         }
     }
-    
+
     public static void Main()
     {
         int x = Convert.ToInt32(Console.ReadLine());
@@ -44,4 +68,4 @@ class Task3
 
         whatNumerOnRight(x, a, b);
     }
-}*/
+}
